ca423155ada4a0cceea1c316b349a87ce5899daa
JackBeevers <113144110+JackBeevers@users.noreply.github.com>
2022-09-29 17:06:12 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿
+int a;
+a = 415;
+int b;
+b = 466;
+int c;
+c = 262;
+int d;
+d = 277;
+int e;
+e = 311;
+int f;
+f = 349;
+int g;
+g = 369;
+int WholeNote = 1000;
+Console.Beep(g, WholeNote  );
+Console.Beep(e, WholeNote );
+Console.Beep(d, WholeNote );
+Console.Beep(e , WholeNote   );
+Console.Beep(d , WholeNote   );
+Console.Beep(c, WholeNote   );
+Console.Beep(d, WholeNote  );
+Console.Beep(c, WholeNote   );
+Console.Beep(e, WholeNote   );
+Console.Beep(c, WholeNote   );
+Console.Beep(d, WholeNote   );
+Console.Beep(b, WholeNote   );
+Console.Beep(b, WholeNote   );
+Console.Beep(a, WholeNote   );
+
+Console.Beep(b, WholeNote  );
+Console.Beep(a, WholeNote   );
+Console.Beep(d, WholeNote  );
+Console.Beep(a, WholeNote  );
+Console.Beep(g, WholeNote   );
+Console.Beep(a, WholeNote   );
+Console.Beep(g, WholeNote   );
+Console.Beep(f, WholeNote );
+Console.Beep(g, WholeNote );
+Console.Beep(e, WholeNote );
+Console.Beep(e, WholeNote );
+Console.Beep(f, WholeNote );
+Console.Beep(e, WholeNote );
+Console.Beep(a, WholeNote );
+
+Console.Beep(e, WholeNote );
+Console.Beep(d, WholeNote );
+Console.Beep(e, WholeNote );
+Console.Beep(d, WholeNote );
+Console.Beep(c, WholeNote );
+Console.Beep(c, WholeNote );
+Console.Beep(b, WholeNote );
+Console.Beep(e, WholeNote );
+Console.Beep(g, WholeNote );
+Console.Beep(e, WholeNote );
+Console.Beep(d, WholeNote );
+Console.Beep(c, WholeNote );
+Console.Beep(d, WholeNote );
+
+Console.Beep(g,WholeNote );
+Console.Beep(e,  WholeNote );
+Console.Beep(f,    WholeNote ); //it should go (f,a,d, WholeNote)
+Console.Beep(e,   WholeNote );
+Console.Beep(d, WholeNote );
+Console.Beep(c,   WholeNote );
+Console.Beep(g, WholeNote ); // it should go (g,a,c, WholeNote)
+Console.Beep(d,   WholeNote );
+Console.Beep(a, WholeNote );
+Console.Beep(g, WholeNote );
+Console.Beep(e, WholeNote );
+
+Console.Beep(d, WholeNote );
+Console.Beep(e, WholeNote );
+Console.Beep(d, WholeNote );
+Console.Beep(c, WholeNote );
+Console.Beep(a, WholeNote );
